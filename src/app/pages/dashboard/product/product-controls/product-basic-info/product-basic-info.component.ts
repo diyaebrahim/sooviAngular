6a716175a58f824c5c00bcce13c63a39059dbb1a
Diyaa Ebrahim <diyaaebrahim@outlook.com>
2021-06-01 10:03:33 +0200

    fix feature

--- src/app/pages/dashboard/product/product-controls/product-basic-info/product-basic-info.component.ts
+++ src/app/pages/dashboard/product/product-controls/product-basic-info/product-basic-info.component.ts
@@ -1,10 +1,11 @@
 import { DatePipe } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
-import { NbToastrService } from "@nebular/theme";
+import { Component, Input, OnInit, TemplateRef } from "@angular/core";
+import { NbDialogService, NbToastrService } from "@nebular/theme";
 import { ProductService } from "../../../../../@core/app-services/product.service";
 import { AddProductModel } from "../../../../../@core/models/addProductModel";
 import { Brand } from "../../../../../@core/models/brand";
 import { Category } from "../../../../../@core/models/category";
+import { Feature } from "../../../../../@core/models/feature";
 import { Grade } from "../../../../../@core/models/grade";
 import { Product } from "../../../../../@core/models/product";
 
@@ -26,6 +27,7 @@ export class ProductBasicInfoComponent implements OnInit {
   get product(): Product {
     return this._product;
   }
+  newFeature: Feature=new Feature();
   loaded: boolean = false;
   grades: Grade[] = new Array();
   brands: Brand[] = new Array();
@@ -34,11 +36,14 @@ export class ProductBasicInfoComponent implements OnInit {
   createdAt: string;
   updatedAt: string;
   selectedProductType: string;
+  features: Feature[] = new Array();
+  filteredFeatures: Feature[] = new Array();
   addOrUpdateModel: AddProductModel = new AddProductModel();
   constructor(
     private productService: ProductService,
     private datepipe: DatePipe,
-    private toastrService: NbToastrService
+    private toastrService: NbToastrService,
+    private dialogService :NbDialogService
   ) {
     this.productService.getBrands().subscribe((data) => {
       this.brands = data;
@@ -48,6 +53,14 @@ export class ProductBasicInfoComponent implements OnInit {
     });
     this.productService.getCategories().subscribe((data) => {
       this.categories = data;
+    });
+    this.productService.getFeatures().subscribe((data) => {
+      this.features = data;
+      this.features.forEach(element => {
+        if(!this.product.features.find(f=>f.id==element.id)){
+            this.filteredFeatures.push(element);
+        }
+      });
     });
   }
 
@@ -75,5 +88,25 @@ export class ProductBasicInfoComponent implements OnInit {
           );
         }
       });
+  }
+  addFeature(ref) {
+    ref.close();
+    this.product.features.push(this.newFeature);
+    this.filteredFeatures = new Array();
+    this.features.forEach(element => {
+      if(!this.product.features.find(f=>f.id==element.id)){
+          this.filteredFeatures.push(element);
+      }
+    });
+  }
+  deleteFeature(feature:Feature){
+    const index = this.product.features.indexOf(feature, 0);
+    if (index > -1) {
+      this.product.features.splice(index, 1);
+      this.filteredFeatures.push(feature);
+    }
+  }
+  openDialog(dialog: TemplateRef<any>) {
+    this.dialogService.open(dialog);
   }
 }
